2c9200b04db358344816e00d65298021bedd9496
Daniel Bostwick <daniel.k.bostwick@berkeley.edu>
2024-04-11 01:26:59 -0700

    Centralizing everything in index.html

---
+++ script.js
@@ -0,0 +1,65 @@
+/*==================================
+        HARVEST VISUAL
+==================================*/
+function openHarvestPopup() {
+  document.getElementById("harvest").style.display = "block";
+}
+
+function closeHarvestPopup() {
+  document.getElementById("harvest").style.display = "none";
+}
+
+/*==================================
+        TASTE VISUAL
+==================================*/
+function openTastePopup() {
+  document.getElementById("taste").style.display = "block";
+}
+
+function closeTastePopup() {
+  document.getElementById("taste").style.display = "none";
+}
+
+/*==================================
+        CONSUMPTION VISUAL
+==================================*/
+function openConsumptionPopup() {
+  document.getElementById("consumption").style.display = "block";
+}
+
+function closeConsumptionPopup() {
+  document.getElementById("consumption").style.display = "none";
+}
+
+/*==================================
+        PRODUCTION VISUAL
+==================================*/
+function openProductionPopup() {
+  document.getElementById("production").style.display = "block";
+}
+
+function closeProductionPopup() {
+  document.getElementById("production").style.display = "none";
+}
+
+/*==================================
+      IMPORT / EXPORT VISUAL
+==================================*/
+function openImpExpPopup() {
+  document.getElementById("impexp").style.display = "block";
+}
+
+function closeImpExpPopup() {
+  document.getElementById("impexp").style.display = "none";
+}
+
+/*==================================
+        TASTE VISUAL
+==================================*/
+// function openTastePopup() {
+//   document.getElementById("taste").style.display = "block";
+// }
+
+// function closeTastePopup() {
+//   document.getElementById("taste").style.display = "none";
+// }
